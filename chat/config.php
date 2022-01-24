02af95665c0ed2c9069c987e33d744002bf348ce
Kovacs Lorand <lorinet3@gmail.com>
2022-01-24 21:36:52 +0200

    Update config.php

--- chat/config.php
+++ chat/config.php
@@ -3,7 +3,7 @@
 define('DB_SERVER', 'localhost');
 define('DB_NAME', 'linfinitype_chat');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'N3BU10NY4ROX');
+define('DB_PASSWORD', 'password');
 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 ?>
